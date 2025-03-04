f763531b639ff929776911a9b6f841192838cf0b
Zinfioj <156579191+Zinfioj@users.noreply.github.com>
2025-03-04 11:59:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 st.title('WELCOM')
-st.write('hãy nhập tên người dùng')
+st.text('hãy nhập tên người dùng')
 name = st.text_input('nhập username')
 if str(name)[:len(name)-6] != 'gay123':
-    st.write('nhập lại tên của bạn')
+    st.text('nhập lại tên của bạn')
     name = st.text_input(':)))')
 
-st.write('nhập mật khẩu')
+st.text('nhập mật khẩu')
 password = st.text_input('password')
 
 if str(password.lower()) != 'taobigay':
-    st.write('nhập lại mật khẩu')
+    st.text('nhập lại mật khẩu')
     password = st.text_input('bạn có bị gay không')
-st.write('chào '+name[:len(name)-6])
+st.text('chào '+name[:len(name)-6])
