ef472d563502dfef621c40b35cfd0231a38b7179
Zinfioj <156579191+Zinfioj@users.noreply.github.com>
2025-03-04 11:50:36 +0700

    app.py

--- app.py
+++ app.py
@@ -1,45 +1,15 @@
 import streamlit as st
-import pandas as pd
+st.title('WELCOM')
+st.write('hãy nhập tên người dùng')
+name = st.text_input('nhập username')
+while str(name)[:len(name)-6] != 'gay123':
+    st.write('nhập lại tên của bạn')
+    name = st.text_input(':)))')
 
-st.title("Thu thập thông tin người dùng")
+st.wrtie('nhập mật khẩu')
+passs = st.text_input('password')
 
-# Thu thập tên
-name = st.text_input("Họ và tên:")
-
-# Thu thập tuổi
-age = st.number_input("Tuổi:", min_value=0, max_value=100, step=1)
-
-# Thu thập giới tính
-gender = st.radio("Giới tính:", ("Nam", "Nữ", "Khác"))
-
-# Thu thập sở thích
-hobbies = st.multiselect(
-    "Sở thích:",
-    ["Đọc sách", "Xem phim", "Chơi thể thao", "Đi du lịch", "Khác"]
-)
-
-# Thu thập ý kiến người dùng
-feedback = st.text_area("Ý kiến của bạn:")
-
-# Xác nhận và hiển thị thông tin đã thu thập
-if st.button("Gửi"):
-    # Lưu thông tin vào tệp CSV
-    user_data = {
-        "Họ và tên": [name],
-        "Tuổi": [age],
-        "Giới tính": [gender],
-        "Sở thích": [", ".join(hobbies)],
-        "Ý kiến": [feedback]
-    }
-    
-    df = pd.DataFrame(user_data)
-    
-    # Ghi thông tin vào tệp CSV (tạo tệp mới nếu chưa tồn tại, bổ sung dữ liệu nếu đã tồn tại)
-    df.to_csv("user_data.csv", mode='a', index=False, header=False)
-    
-    st.write("Thông tin đã được lưu thành công!")
-    st.write("Họ và tên:", name)
-    st.write("Tuổi:", age)
-    st.write("Giới tính:", gender)
-    st.write("Sở thích:", ", ".join(hobbies))
-    st.write("Ý kiến của bạn:", feedback)
+while str(passs.lower()) != 'taobigay':
+    st.write('nhập lại mật khẩu')
+    pass = st.text_input('bạn có bị gay không')
+st.write('chào '+name[:len(name)-6])
